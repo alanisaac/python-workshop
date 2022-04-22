8d0cdee55859489d0c711e9458999d8784e86f84
Alan Pinkert <alan.pinkert@gmail.com>
2022-04-22 17:58:38 -0400

    removed extra time

--- src/distance_matrix/inputs.py
+++ src/distance_matrix/inputs.py
@@ -1,6 +1,4 @@
-import asyncio
 import csv
-import time
 from typing import AsyncIterable, Iterable, Sequence
 
 from .models.coordinates import Coordinates
@@ -15,7 +13,6 @@ def read_input(path: str) -> Iterable[Location]:
     with open(path, "r") as input_file:
         for row in csv.reader(input_file):
             location = _parse_input(row)
-            time.sleep(0.01)
             yield location
 
 
@@ -23,5 +20,4 @@ async def read_input_async(path: str) -> AsyncIterable[Location]:
     with open(path, "r") as input_file:
         for row in csv.reader(input_file):
             location = _parse_input(row)
-            await asyncio.sleep(0.01)
             yield location
--- src/distance_matrix/outputs.py
+++ src/distance_matrix/outputs.py
@@ -1,6 +1,4 @@
-import asyncio
 import csv
-import time
 from typing import AsyncIterable, Iterable
 
 from .models.output import Output
@@ -10,7 +8,6 @@ def write_output(path: str, output_records: Iterable[Output]) -> None:
     with open(path, "w", newline="") as output_file:
         writer = csv.writer(output_file)
         for record in output_records:
-            time.sleep(0.01)
             writer.writerow((record.origin, record.destination, record.distance))
 
 
@@ -18,5 +15,4 @@ async def write_output_async(path: str, output_records: AsyncIterable[Output]) -
     with open(path, "w", newline="") as output_file:
         writer = csv.writer(output_file)
         async for record in output_records:
-            await asyncio.sleep(0.01)
             writer.writerow((record.origin, record.destination, record.distance))
